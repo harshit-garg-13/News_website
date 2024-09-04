5c67373db1ee01d26e422a8fe370b97d28f81d57
harshit-garg-13 <garg.harshit1303@gmail.com>
2024-09-05 01:21:35 +0530

    hello

--- src/App.js
+++ src/App.js
@@ -17,21 +17,21 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <Router basename="/New-Website">
+        <Router>
         <Navbar/>
         <LoadingBar
         color='#f11946'
         progress={this.state.progress}
       />
         <Routes>
-        <Route  exact path="/New-Website" element={   <News setProgress={this.setProgress}  key="general" country="in" category="general" name="Latest News"/>}/>
-         <Route exact path="/sports" element={   <News setProgress={this.setProgress}  key="sports" country="in" category="sports" name="Sports news"/>}/>
-         <Route exact path="/business" element={   <News setProgress={this.setProgress}  key="business" country="in" category="business" name="Business news"/>}/>
-         <Route exact path="/entertainment" element={   <News setProgress={this.setProgress}  key="entertainment" country="in" category="entertainment" name="Entertainment News "/>}/>
-         <Route exact path="/science" element={   <News setProgress={this.setProgress}  key="science" country="in" category="science" name="Science News"/>}/>
-         <Route exact path="/technology" element={   <News setProgress={this.setProgress}  key="technology" country="in" category="technology" name="Technology News"/>}/>
-         <Route exact path="/health" element={   <News setProgress={this.setProgress}  key="health" country="in" category="health" name="Health News"/>}/>
-         <Route exact path="/general" element={   <News setProgress={this.setProgress}  key="general" country="in" category="general" name="Latest News"/>}/>
+        <Route  exact path="/New-Website" element={   <News setProgress={this.setProgress}  key="general" country="us" category="general" name="Latest News"/>}/>
+         <Route exact path="/sports" element={   <News setProgress={this.setProgress}  key="sports" country="us" category="sports" name="Sports news"/>}/>
+         <Route exact path="/business" element={   <News setProgress={this.setProgress}  key="business" country="us" category="business" name="Business news"/>}/>
+         <Route exact path="/entertainment" element={   <News setProgress={this.setProgress}  key="entertainment" country="us" category="entertainment" name="Entertainment News "/>}/>
+         <Route exact path="/science" element={   <News setProgress={this.setProgress}  key="science" country="us" category="science" name="Science News"/>}/>
+         <Route exact path="/technology" element={   <News setProgress={this.setProgress}  key="technology" country="us" category="technology" name="Technology News"/>}/>
+         <Route exact path="/health" element={   <News setProgress={this.setProgress}  key="health" country="us" category="health" name="Health News"/>}/>
+         <Route exact path="/general" element={   <News setProgress={this.setProgress}  key="general" country="us" category="general" name="Latest News"/>}/>
         </Routes>
       
        </Router>
